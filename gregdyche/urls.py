e1739ae3adacf50e9525f10f19beb3c17c44ef61
Greg Dyche <gregdyche@creighton.edu>
2025-06-08 21:33:41 -0500

    add home function to urls

--- gregdyche/urls.py
+++ gregdyche/urls.py
@@ -1,17 +1,44 @@
 # gregdyche/urls.py
 
+"""
+URL configuration for gregdyche project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+"""
+
 from django.contrib import admin
-from django.urls import path, include  # <-- Make sure to import 'include'
+from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 
-# ... (your home and debug_view functions) ...
+# This function must be defined to be used below
+def home(request):
+    return HttpResponse("Hello, Greg. This is your Railway app.")
+
+# This function must also be defined to be used below
+def debug_view(request):
+    db_host = settings.DATABASES['default'].get('HOST', 'Not Found')
+    output = f"<h1>Debug Information</h1>"
+    output += f"<p><b>Database Host the web app is connected to:</b> {db_host}</p>"
+    
+    output += "<hr><h2>Session Write Test</h2>"
+    try:
+        request.session['debug_test'] = 'value_was_set'
+        request.session.save()
+        output += "<p style='color:green;'><b>Session Write Test: ✅ SUCCESS</b></p>"
+        output += "<p>The application successfully wrote a value to the session table.</p>"
+    except Exception as e:
+        output += f"<p style='color:red;'><b>Session Write Test: ❌ FAILED</b></p>"
+        output += f"<p><b>This is the root cause of the login issue.</b></p>"
+        output += f"<p><b>Error returned:</b> {e}</p>"
+        
+    return HttpResponse(output)
+
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('debug-view/', debug_view, name='debug_view'),
-    
-    # --- ADD THIS LINE ---
     path('blog/', include('blog.urls', namespace='blog')),
 ]
